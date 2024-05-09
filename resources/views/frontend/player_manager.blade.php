d122817548fed5a429770d0b66d326df10df3a3e
sang.le <sl4927876@gmail.com>
2024-05-09 11:26:09 +0700

    update

--- resources/views/frontend/player_manager.blade.php
+++ resources/views/frontend/player_manager.blade.php
@@ -14,28 +14,37 @@
                     <h3 class="fs-24 font-weight-semi-bold pb-2">Player Manager <b
                             class="text-success">{{$playerip}}</b></h3>
                     <div class="divider"><span></span></div>
-                    <div class="col-lg-4">
-                        <ul class="list-group">
-                            <li class="list-group-item">
-                                <a href="http://{{$playerip}}:1400/reset" class="btn btn-danger" target="_blank">Reset</a>
-                            </li>
-                            <li class="list-group-item">
-                                <a href="http://{{$playerip}}:1400/reboot" class="btn btn-danger" target="_blank">Reboot</a>
-                            </li>
-                            <li class="list-group-item">
-                                <a href="http://{{$playerip}}:1400/status/zp" class="btn btn-danger" target="_blank">Player Info</a>
-                            </li>
-                            <li class="list-group-item">
-                                <a href="http://{{$playerip}}:1400/status" class="btn btn-danger" target="_blank">Status</a>
-                            </li>
-                            <li class="list-group-item">
-                                <a href="http://{{$playerip}}:1400/status/trueplayinfo" class="btn btn-danger" target="_blank">Trueplay info</a>
-                            </li>
-                        </ul>
-                    </div>
-                    <div class="col-lg-8">
+                    <div class="row">
+                        <div class="col-lg-6">
+                            {{--                        <ul class="list-group">--}}
+                            {{--                            <li class="list-group-item">--}}
+                            {{--                                <a href="http://{{$playerip}}:1400/reset" class="btn btn-danger" target="_blank">Reset</a>--}}
+                            {{--                            </li>--}}
+                            {{--                            <li class="list-group-item">--}}
+                            {{--                                <a href="http://{{$playerip}}:1400/reboot" class="btn btn-danger" target="_blank">Reboot</a>--}}
+                            {{--                            </li>--}}
+                            {{--                            <li class="list-group-item">--}}
+                            {{--                                <a href="http://{{$playerip}}:1400/status/zp" class="btn btn-danger" target="_blank">Player Info</a>--}}
+                            {{--                            </li>--}}
+                            {{--                            <li class="list-group-item">--}}
+                            {{--                                <a href="http://{{$playerip}}:1400/status" class="btn btn-danger" target="_blank">Status</a>--}}
+                            {{--                            </li>--}}
+                            {{--                            <li class="list-group-item">--}}
+                            {{--                                <a href="http://{{$playerip}}:1400/status/trueplayinfo" class="btn btn-danger" target="_blank">Trueplay info</a>--}}
+                            {{--                            </li>--}}
+                            {{--                        </ul>--}}
+                            <iframe src="http://{{$playerip}}:1400/reset" title="Reset" frameborder="0"></iframe>
+                            <iframe src="http://{{$playerip}}:1400/reboot" title="Reset" frameborder="0"></iframe>
+                            <hr>
+                            <iframe src="http://{{$playerip}}:1400/setstring" title="Reset" width="100%" height="400px" frameborder="0"></iframe>
 
+                        </div>
+
+                        <div class="col-lg-6">
+                            <iframe src="http://{{$playerip}}:1400/status/zp" title="Reset" frameborder="0" width="100%" height="1000px"></iframe>
+                        </div>
                     </div>
+
                 </div><!-- end card-body -->
             </div><!-- end card -->
         </div><!-- end col-lg-5 -->
