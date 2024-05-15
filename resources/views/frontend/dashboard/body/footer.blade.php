199125783d588b0edb9c99aecaf10f36a2e785b5
sang.le <sl4927876@gmail.com>
2024-05-15 16:06:05 +0700

    update

--- resources/views/frontend/dashboard/body/footer.blade.php
+++ resources/views/frontend/dashboard/body/footer.blade.php
@@ -1,4 +1,4 @@
 <footer class="page-footer text-center">
-    <p class="mb-0">Copyright © 2024. Luyện Thi Công Chức. All right reserved.</p>
+    <p class="mb-0">Copyright © 2024. Sonos. All right reserved.</p>
 </footer>
 
